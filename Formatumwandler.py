d24494699657ae41485584207e8600772bcd8eb1
Eliah Hueber (PC Home) <@huebis>
2025-03-06 19:15:52 +0100

    AddWasserstoff probiert selbst zu schreiben und damit eine externe libary weniger zu brauchen, aber es hat immer noch mehrere fehler

--- Formatumwandler.py
+++ Formatumwandler.py
@@ -21,3 +21,71 @@ def AddWasserstoff(isomerearray):
         molekulobject = AddHs(molekulobject)
         isomerearray[t] = Chem.MolToSmiles(molekulobject)
     return isomerearray
+
+def AddWasserstoff2():
+    isomere = 'O1C2CC3(OC3C#C1)C2'
+
+    def AnzahlAtombindungen(char):
+        if char == 'O':
+            return 2
+        if char == 'C':
+            return 4
+        if char == 'c':
+            return 3
+    def Bindungen(char):
+        if char == '=':
+            return 2
+        if char == '#':
+            return 3
+        return 1  #Wenn nicht ein = oder ein # kommen muss vorhin eine Zahl, ein Atom oder eine Klammer kommen, dies impliziert dann direkt, dass es automatisch ein einfach Bindung ist
+
+
+    for a in range (len(isomere)):
+        if isomere[a] == 'C' or isomere[a] == 'c' or isomere[a] == 'O':
+            anzahlBindungen = AnzahlAtombindungen(isomere[a])
+            print("Hallo")
+            print("a:" + str(a))
+
+            bindungenohneWasserstoff = 0
+            if a > 0:
+                bindungenohneWasserstoff += Bindungen(isomere[a-1])
+
+
+
+
+            for b in range(1,len(isomere)-a,1):
+                if not isomere[a+b].isdigit():
+                    c = 0
+                    if isomere[a+b] == '(':
+                        bindungenohneWasserstoff = Bindungen(isomere[a + b+1])
+                        c += 1
+                        anzahlklammern = 1 # nach rechts offene Klammer ist +1, nach links offene -1
+                        while(True):
+                            c += 1
+                            if isomere[a+b+c] == '(':
+                                anzahlklammern += 1
+                            if isomere[a+b+c] == ')':
+                                anzahlklammern -= 1
+                                if anzahlklammern == 0:
+                                    if not isomere[a+b+c+1] == '(':
+                                        c += 1
+                                        break
+                                    else:
+                                        bindungenohneWasserstoff = Bindungen(isomere[a + b + c + 2])
+                                        c += 3
+                                        anzahlklammern = 1  # nach rechts offene Klammer ist +1, nach links offene -1
+
+
+
+                    if isomere[a+b+c] == ')':
+                        break
+                    else:
+                        bindungenohneWasserstoff += Bindungen(isomere[a+b+c])
+                        break
+
+            print(anzahlBindungen -bindungenohneWasserstoff)
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import Formatumwandler
 
 isomerearray = Formatumwandler.Readfile()
-isomerearray = Formatumwandler.AddWasserstoff(isomerearray)
-print(isomerearray)
+#isomerearray = Formatumwandler.AddWasserstoff(isomerearray)
+Formatumwandler.AddWasserstoff2()
+#print(isomerearray)
